4f547cdfae172a8939f4406ec0dcbd6a2f21e9f2
Caio Lucena <caiodelucena6@gmail.com>
2022-10-18 22:10:48 -0300

    feat: sidekiq config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
+require 'sidekiq/web'
+
+# Configure Sidekiq-specific session middleware
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   root :to => "index#index"
+
   namespace :api do 
     namespace :v1 do
       resources :articles
